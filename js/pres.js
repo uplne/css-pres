d6838a86661f121f680534844167d736a680b21b
Steve Vadocz <uplneinak@gmail.com>
2014-02-02 17:38:01 +0000

    Initial setup.

---
+++ js/pres.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    var options = {};
+
+    options.steps = {
+        "intro": {
+            x: 0, y: 0
+        },
+        "intro-name": {
+            x: 0, y: 0
+        }
+    };
+
+    impress().init();
+}());
